0c898972c4965f4eca3c2654e36b3547ef3ad357
Grégory Starck <gregory.starck@savoirfairelinux.com>
2014-09-29 17:04:03 -0400

    Change: the base run-tests.sh script to run all scripts and reports what has then failed.

--- tools/run-tests.sh
+++ tools/run-tests.sh
@@ -2,6 +2,10 @@
 set -e
 export GREP_OPTIONS=""
 
+declare -i n_done=0
+declare -i n_failed=0
+declare failed=""
+
 # Plugins
 cd plugins
 for plugin in `ls -d */`; do
@@ -10,15 +14,30 @@ for plugin in `ls -d */`; do
     # workaround, as long as we still have these dirty jenkins scripts
     if [ -e $file ]
     then
-        $file
+        "$file"
     else
         ./run_tests.sh
-    fi
+    fi || {
+        n_failed+=1
+        failed="$failed\n${plugin}"
+    }
+    n_done+=1
     cd ..
 done
 cd ..
 
 # finally: shinkenplugins
 cd shinkenplugins
-./run_tests.sh
+./run_tests.sh || {
+    shinkenplugins_failed=1
+}
 cd ..
+
+cat << END
+Final report: $n_done plugins checked ; $n_failed failed.
+Following plugins failed: $(echo -e $failed)
+END
+if test "$shinkenplugins_failed"
+then
+    echo "More over the shinkenplugins package test itself failed"
+fi
